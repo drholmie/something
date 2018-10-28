ff00e2fef4041bb98e3a6cb185b1d6eb313613ee
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-10-29 00:09:13 +0530

    SignUp and Login Page improvements

--- src/Login.js
+++ src/Login.js
@@ -14,16 +14,20 @@ export default class Login extends React.Component {
     var self = this;
     const { email, password } = self.state;
     self.setState({ loading: true });
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        self.setState({ loading: false});
-        self.props.navigation.navigate('App');
-      })
-      .catch(error => {
-        self.setState({ errorMessage: error.message, email: '', password: '', loading: false });
-      })
+    if (!email || !password)
+      self.setState({ errorMessage: "Input fields cannot be blank.", loading: false });
+    else {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          self.setState({ loading: false });
+          self.props.navigation.navigate('App');
+        })
+        .catch(error => {
+          self.setState({ errorMessage: error.message, email: '', password: '', loading: false });
+        })
+    }
   }
 
   render() {
@@ -32,31 +36,40 @@ export default class Login extends React.Component {
     else
       return (
         <View style={styles.container}>
-          <Text>Login</Text>
+          <Text style={[styles.textstyles, { textAlign: 'center' }]}>Login</Text>
           {this.state.errorMessage &&
-            <Text style={{ color: 'red' }}>
+            <Text style={{ color: 'red', textAlign: 'auto' }}>
               {this.state.errorMessage}
             </Text>}
-          <TextInput
-            style={styles.textInput}
-            autoCapitalize="none"
-            placeholder="Email"
-            onChangeText={email => this.setState({ email })}
-            value={this.state.email}
-          />
-          <TextInput
-            secureTextEntry
-            style={styles.textInput}
-            autoCapitalize="none"
-            placeholder="Password"
-            onChangeText={password => this.setState({ password })}
-            value={this.state.password}
-          />
-          <Button title="Login" onPress={this.handleLogin} />
-          <Button
-            title="Don't have an account? Sign Up"
-            onPress={() => this.props.navigation.navigate('SignUp')}
-          />
+          <View style={styles.miniContainer}>
+            <TextInput
+              style={styles.textInput}
+              autoCapitalize="none"
+              placeholder="Email"
+              placeholderTextColor="white"
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            />
+            <TextInput
+              secureTextEntry
+              style={styles.textInput}
+              autoCapitalize="none"
+              placeholder="Password"
+              placeholderTextColor="white"
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+            />
+            <View style={styles.buttonstyles}>
+              <Button title="Login" color="#cd5c5c" onPress={this.handleLogin} />
+            </View>
+            <View style={styles.buttonstyles}>
+              <Button
+                color="#cd5c5c"
+                title="Don't have an account? Sign Up"
+                onPress={() => this.props.navigation.navigate('SignUp')}
+              />
+            </View>
+          </View>
         </View>
       )
   }
@@ -66,13 +79,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  miniContainer: {
+    justifyContent: 'center',
     alignItems: 'center'
   },
   textInput: {
     height: 40,
     width: '90%',
-    borderColor: 'gray',
+    color: 'white',
+    backgroundColor: '#cd5c5c',
+    borderColor: '#cd5c5c',
     borderWidth: 1,
-    marginTop: 8
+    marginVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 25,
+  },
+  textstyles: {
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'sans-serif-thin',
+  },
+  buttonstyles: {
+    marginVertical: 8
   }
 })
+
--- src/SignUp.js
+++ src/SignUp.js
@@ -14,47 +14,60 @@ export default class SignUp extends React.Component {
     var self = this;
     const { email, password } = this.state;
     self.setState({ loading: true });
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(user => {
-        self.setState({ loading: false });
-        this.props.navigation.navigate('App');
-      })
-      .catch(error => self.setState({ errorMessage: error.message, loading: false }))
+    if (!email || !password)
+      self.setState({ errorMessage: "Input fields cannot be blank.", loading: false });
+    else {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          self.setState({ loading: false });
+          this.props.navigation.navigate('App');
+        })
+        .catch(error => self.setState({ errorMessage: error.message, loading: false }))
+    }
   }
 
   render() {
-    if(this.state.loading)
+    if (this.state.loading)
       return <Loading />;
     else
       return (
         <View style={styles.container}>
-          <Text>Sign Up</Text>
+          <Text style={[styles.textstyles, { textAlign: 'center' }]}>Sign Up</Text>
           {this.state.errorMessage &&
-            <Text style={{ color: 'red' }}>
+            <Text style={{ color: 'red', textAlign: 'auto' }}>
               {this.state.errorMessage}
             </Text>}
-          <TextInput
-            placeholder="Email"
-            autoCapitalize="none"
-            style={styles.textInput}
-            onChangeText={email => this.setState({ email })}
-            value={this.state.email}
-          />
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            autoCapitalize="none"
-            style={styles.textInput}
-            onChangeText={password => this.setState({ password })}
-            value={this.state.password}
-          />
-          <Button title="Sign Up" onPress={this.handleSignUp} />
-          <Button
-            title="Already have an account? Login"
-            onPress={() => this.props.navigation.navigate('Login')}
-          />
+          <View style={styles.miniContainer}>
+            <TextInput
+              placeholder="Email"
+              autoCapitalize="none"
+              placeholderTextColor="white"
+              style={styles.textInput}
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            />
+            <TextInput
+              secureTextEntry
+              placeholderTextColor="white"
+              placeholder="Password"
+              autoCapitalize="none"
+              style={styles.textInput}
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+            />
+            <View style={styles.buttonstyles}>
+              <Button color="#cd5c5c" title="Sign Up" onPress={this.handleSignUp} />
+            </View>
+            <View style={styles.buttonstyles}>
+              <Button
+                color="#cd5c5c"
+                title="Already have an account? Login"
+                onPress={() => this.props.navigation.navigate('Login')}
+              />
+            </View>
+          </View>
         </View>
       )
   }
@@ -64,13 +77,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  miniContainer: {
+    justifyContent: 'center',
     alignItems: 'center'
   },
   textInput: {
     height: 40,
     width: '90%',
-    borderColor: 'gray',
+    color: 'white',
+    backgroundColor: '#cd5c5c',
+    borderColor: '#cd5c5c',
     borderWidth: 1,
-    marginTop: 8
+    marginVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 25,
+  },
+  textstyles: {
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'sans-serif-thin',
+  },
+  buttonstyles: {
+    marginVertical: 8
   }
 })
